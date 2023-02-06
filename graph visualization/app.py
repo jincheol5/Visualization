dcb19696369e0c17ec436dd27ea9a6f0490131ce
jincheol <oscar4293@gmail.com>
2023-02-06 22:31:11 +0900

    mod 230206

--- graph visualization/app.py
+++ graph visualization/app.py
@@ -1,13 +1,26 @@
-import pandas as pd 
-from pandas.plotting import parallel_coordinates
-from event import Event
-from data_set import data_set
-import networkx as nx
 import matplotlib.pyplot as plt
-from visualization import draw_bipartite
+import numpy as np
 
-data=data_set("data-10.txt")
 
-parallel_coordinates(data.get_dataframe(),class_column="unix_time")
+y=['a','b','c','d','e']
+
+x_list=[[1,2],[2,3],[3,4],[4,5]]
+
+
+y_list=[['a','b'],['a','c'],['b','d'],['d','a']]
+
+
+fig,ax_s=plt.subplots(ncols=4)
+
+i=0
+for ax in ax_s:
+    ax.set_ylim(y)
+    ax.plot(x_list[i],y_list[i])
+    i+=1
+
+
+fig.subplots_adjust(wspace=0)
+
+
 
 plt.show()
