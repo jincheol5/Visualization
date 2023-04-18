573d61616de3a1bad5893c49cdef03592682323e
jincheol <oscar4293@gmail.com>
2023-04-18 17:11:54 +0900

    update 230418

--- visualization/main.py
+++ visualization/main.py
@@ -9,8 +9,10 @@ edges=[]
 unixtimes=[]
 
 #data 불러오기 
-file=open('graph visualization/data/data-1000.csv','r')
+file=open('visualization/data/data-1000.csv','r')
 lines=csv.reader(file)
+
+
 
 for line in lines:
     #coloum 제거
@@ -29,24 +31,122 @@ for line in lines:
     edges.append(event(label,line[0],line[1],line[2]))
     
 
+#unixtimes.append(0) #시작 시간 추가 
 unixtimes.sort() #unixtime 오름차순 정렬
+nodes=list(nodes) #node set -> node list 
+
 
 #visualization
 
-# Create a graph
-G = nx.Graph()
+# create a graph
+G=nx.Graph()
 
-# Add some nodes to the graph
-G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
+time_nodes={} #기준 시간축 노드들
 
-# Add some edges to the graph
-G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4), (4, 5), (4, 6), (5, 7), (6, 8), (7, 8)])
+time_value=100000 #시간 축 간격 기준, 기준이 너무 크면 오류 
+
+max_unixtime=max(unixtimes)
+min_unixtime=min(unixtimes)
+
+
+
+time_axis=min_unixtime
+
+#각 시간축마다 노드들 이름 변경하여 추가 
+while(time_axis<max_unixtime):
+    new_nodes=[]
+    for t in range(len(nodes)):
+        new_nodes.append(nodes[t]+"-"+str(time_axis))
+    time_nodes[time_axis]=new_nodes
+    time_axis+=time_value
+    
+
+
+#그래프에 node 추가 
+time_axis=min_unixtime
+while(time_axis<max_unixtime):
+    G.add_nodes_from(time_nodes[time_axis])
+    time_axis+=time_value
+
+
+#그래프에 edge 추가
+for edge in edges:
+    time_index=int((int(edge.unixtime)-min_unixtime)/time_value)
+    if time_index==0:
+        G.add_edge(edge.source+"-"+str(min_unixtime),edge.target+"-"+str(min_unixtime+time_value))
+    else:
+        G.add_edge(edge.source+"-"+str(min_unixtime+(time_index*time_value-time_value)),edge.target+"-"+str(min_unixtime+time_index*time_value))
+    
+
+    
+
+
 
 # Define a custom layout
-pos = {1: (0, 0), 2: (1, 1), 3: (-1, 1), 4: (2, 0), 5: (3, 1), 6: (2, -1), 7: (4, 1), 8: (4, -1)}
+pos = {}
 
+x_axis=0
+time_axis=min_unixtime
+
+while(time_axis<max_unixtime):
+    for index in range(len(time_nodes[time_axis])):
+        pos[time_nodes[time_axis][index]]=(x_axis,index)
+        
+    x_axis+=10000
+    time_axis+=time_value
+    
 # Draw the graph with the custom layout
-nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=14, font_weight='bold')
+nx.draw(G,pos,node_size=1)
 
 # Show the plot
 plt.show()
+
+
+
+
+
+
+
+
+##모든 시간을 시간축으로 할 경우 
+
+# Create a graph
+# G = nx.Graph()
+
+# #time nodes
+# time_nodes={}
+
+# for unixtime in unixtimes:
+#     new_nodes=[]
+#     for t in range(len(nodes)):
+#         new_nodes.append(nodes[t]+"-"+str(unixtime))
+#     time_nodes[unixtime]=new_nodes
+    
+
+
+
+# #insert node
+# for unixtime in unixtimes:
+#     G.add_nodes_from(time_nodes[unixtime])
+    
+    
+# # Define a custom layout
+# pos = {}
+
+# x_axis=0
+
+# for unixtime in unixtimes:
+#     for index in range(len(time_nodes[unixtime])):
+#         pos[time_nodes[unixtime][index]]=(x_axis,index)
+
+#     x_axis+=10000
+
+
+
+
+
+# # Draw the graph with the custom layout
+# nx.draw(G,pos,node_size=1)
+
+# # Show the plot
+# plt.show()
